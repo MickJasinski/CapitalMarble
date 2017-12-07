e83f1fbf18ea152dafae6490a62de3b2a529c456
gaap <baq@lost-sounds.com>
2017-12-07 17:56:40 +0000

    Add carousel

--- js/js.js
+++ js/js.js
@@ -1,3 +1,5 @@
+//==== Mobile Nav ====
+
 $(document).ready(function () {
 
     $(".cross").hide();
@@ -16,4 +18,33 @@ $(document).ready(function () {
         });
     });
 
+});
+
+
+//==== Carousel ====
+
+var $item = $('.carousel .item');
+var $wHeight = $(window).height();
+$item.eq(0).addClass('active');
+$item.height($wHeight);
+$item.addClass('full-screen');
+
+$('.carousel img').each(function () {
+    var $src = $(this).attr('src');
+    var $color = $(this).attr('data-color');
+    $(this).parent().css({
+        'background-image': 'url(' + $src + ')',
+        'background-color': $color
+    });
+    $(this).remove();
+});
+
+$(window).on('resize', function () {
+    $wHeight = $(window).height();
+    $item.height($wHeight);
+});
+
+$('.carousel').carousel({
+    interval: 6000,
+    pause: "false"
 });
