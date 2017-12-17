2f8249e63fe0b64a3ccd9af527ff2e925f60e23e
Mick Jasinski <m.jasinski@minimalcraft.net>
2017-12-17 13:08:31 +0000

    Move scripts to js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+    // Hamburger
+    $(document).ready(function() {
+        $(".cross").hide();
+        $(".menu").hide();
+        $(".hamburger").click(function() {
+            $(".menu").slideToggle("slow", function() {
+                $(".hamburger").hide();
+                $(".cross").show();
+            });
+        });
+        $(".cross").click(function() {
+            $(".menu").slideToggle("slow", function() {
+                $(".cross").hide();
+                $(".hamburger").show();
+            });
+        });
+    });
+    //==== Carousel ====
+    var $item = $('.carousel .item');
+    var $wHeight = $(window).height();
+    $item.eq(0).addClass('active');
+    $item.height($wHeight);
+    $item.addClass('full-screen');
+    $('.carousel img').each(function() {
+        var $src = $(this).attr('src');
+        var $color = $(this).attr('data-color');
+        $(this).parent().css({
+            'background-image': 'url(' + $src + ')',
+            'background-color': $color
+        });
+        $(this).remove();
+    });
+    $(window).on('resize', function() {
+        $wHeight = $(window).height();
+        $item.height($wHeight);
+    });
+    $('.carousel').carousel({
+        interval: 6000,
+        pause: "false"
+    });
